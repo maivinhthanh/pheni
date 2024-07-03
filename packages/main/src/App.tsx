2deca54c76fa290f9552e47537a4023a7e32d4d0
Mai Vinh Thanh <maivinhthanh@Mai-Vinh-Thanh-Mac-Pro.local>
2024-07-03 23:02:58 +0700

    init project

---
+++ packages/main/src/App.tsx
@@ -0,0 +1,16 @@
+import { Home } from "home";
+import { About } from "about";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Main Application</h1>
+        <Home />
+        <About />
+      </div>
+    </>
+  );
+}
+
+export default App;
